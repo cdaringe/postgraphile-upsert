8190bdb30f090f7aac54b49e5ac6034ffdeb98cf
ioancole <ioan.coleman@gmail.com>
2020-09-16 12:39:15 +0800

    feat: Support omit tags

--- src/postgraphile-upsert.ts
+++ src/postgraphile-upsert.ts
@@ -17,6 +17,7 @@ const PgMutationUpsertPlugin: Plugin = builder => {
         GraphQLString
       },
       inflection,
+      pgOmit: omit,
       newWithHooks,
       parseResolveInfo,
       pgGetGqlInputTypeByTypeIdAndModifier,
@@ -36,6 +37,7 @@ const PgMutationUpsertPlugin: Plugin = builder => {
       fields,
       pgIntrospectionResultsByKind.class
         .filter((table: any) => !!table.namespace)
+        .filter((table: any) => !omit(table, 'upsert'))
         .filter((table: any) => table.isSelectable)
         .filter((table: any) => table.isInsertable)
         .filter((table: any) => table.isUpdatable)
